8b0c389917ddbb0e68edac29c5eaebcbe593f3b4
Sergey Gulaga <ry6ens@gmail.com>
2022-07-11 16:49:12 +0300

    add task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,18 @@
+import Vimeo from '@vimeo/player';
+const throttle = require('lodash.throttle');
 
+const iframe = document.querySelector('iframe');
+const player = new Vimeo(iframe);
+
+const onPlay = function (data) {
+  localStorage.setItem('videoplayer-current-time', JSON.stringify(data));
+  console.log(parseData.seconds);
+};
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+const savedData = localStorage.getItem('videoplayer-current-time');
+
+const parseData = JSON.parse(savedData);
+
+player.setCurrentTime(parseData.seconds);
