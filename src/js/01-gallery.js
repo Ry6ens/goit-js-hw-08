eeeb60860384f7322d42cbb0040294a218bc72dc
Sergey Gulaga <ry6ens@gmail.com>
2022-07-11 15:04:15 +0300

    add task 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,4 +2,33 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+// Описан в документации
+import SimpleLightbox from 'simplelightbox';
+// Дополнительный импорт стилей
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const galleryList = document.querySelector('.gallery');
+
+const galleryCardsArr = galleryItems
+  .map(
+    el =>
+      `<div >
+        <a href="${el.original}">
+            <img
+            class="gallery__image"
+            src="${el.preview}"
+            alt="${el.description}"
+            data-source="${el.original}"
+            />
+        </a>
+    </div>`
+  )
+  .join('');
+
+galleryList.innerHTML = galleryCardsArr;
+
+new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+});
